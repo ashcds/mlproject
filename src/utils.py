f6ce0defa2bdaa21afa290da72707decfeab673a
Aishwarya <aishwarya.cherian@hotmail.com>
2025-02-20 17:52:13 -0500

    flask app with predict pipeline

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import pandas as pd
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+import pickle
 
 def save_object(file_path, obj): 
     try: 
@@ -48,6 +49,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models, param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
